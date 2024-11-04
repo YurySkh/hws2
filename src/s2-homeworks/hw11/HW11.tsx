e0953a0145534a1db57e650c02f3ec55737a3917
yurysukhan <yurysukhan@gmail.com>
2024-11-04 19:00:14 +0300

    HW №11 is done

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
-import s from './HW11.module.css'
+import { useState } from 'react'
 import s2 from '../../s1-main/App.module.css'
 import { restoreState } from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
+import s from './HW11.module.css'
 
-/*
+/* ❌✅
 * 1 - передать значения в оба слайдера
 * 2 - дописать типы и логику функции change
 * 3 - сделать стили в соответствии с дизайном
@@ -15,8 +15,14 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
+    const change = (event: Event, value: number | number[]) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+        if(Array.isArray(value)) {
+            setValue1(value[0])
+            setValue2(value[1])
+        } else {
+            setValue1(value)
+        }
     }
 
     return (
@@ -29,6 +35,8 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
+                            onChange={change}
+                            value={value1}
                             // сделать так чтоб value1 изменялось // пишет студент
 
                         />
@@ -37,6 +45,8 @@ function HW11() {
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
+                            onChange={change}
+                            value={[value1, value2]}
                             // сделать так чтоб value1/2 изменялось // пишет студент
 
                         />
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,7 +5,25 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                color: 'lightGreen', // Цвет слайдера
+                width: 500,
+                '& .MuiSlider-thumb': { // Стили для "большого пальца" слайдера
+                    backgroundColor: 'lightGreen',
+                    height: 24,
+                    width: 24,
+                    '&:hover': {
+                        boxShadow: 'inherit', // Тень при наведении
+                    },
+                },
+                '& .MuiSlider-track': { // Стили для дорожки слайдера
+                    height: 8,
+                    borderRadius: 4,
+                },
+                '& .MuiSlider-rail': { // Стили для рельса слайдера
+                    height: 8,
+                    borderRadius: 4,
+                    backgroundColor: 'grey',
+                },
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
